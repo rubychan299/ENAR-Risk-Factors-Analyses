1c31c2cc6fef16cfafc5505049e0151d6969bcaa
Yuyu(Ruby) Chen <rubychan299@gmail.com>
2025-03-11 13:15:18 -0400

    Updated final data (2013-2020) with the new feature list

--- R/DataCleaning_FinalFeatures.R
+++ R/DataCleaning_FinalFeatures.R
@@ -1,6 +1,8 @@
 library(readxl)
 library(readr)
 
+load("~/Documents/GitHub/ENAR-Risk-Factors-Analyses/data/cleaned/dat_hyp_mice.RData")
+
 # Use only JNC7 features
 features_en_jnc_2013 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/top_features_2013.csv")
 features_en_jnc_2015 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/top_features_2015.csv")
@@ -10,24 +12,24 @@ features_en_jnc <- bind_cols(features_en_jnc_2013, features_en_jnc_2015, feature
   mutate(across(everything(), ~ str_remove(.x, ".Yes$")))
 colnames(features_en_jnc) <- c("2013", "2015", "2017")
 
-features_xgb_jnc_2013 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/final_features_for_gb_2013.csv")
-features_xgb_jnc_2015 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/final_features_for_gb_2015.csv")
-features_xgb_jnc_2017 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/final_features_for_gb_2017.csv")
-
-fearures_xgb_jnc <- bind_cols(features_xgb_jnc_2013, features_xgb_jnc_2015, features_xgb_jnc_2017) %>% 
-  dplyr::select(-contains("Count")) %>% 
-  mutate(across(everything(), ~ str_remove(.x, "_Yes$")))
-
-colnames(fearures_xgb_jnc) <- c("2013", "2015", "2017")
+# features_xgb_jnc_2013 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/final_features_for_gb_2013.csv")
+# features_xgb_jnc_2015 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/final_features_for_gb_2015.csv")
+# features_xgb_jnc_2017 <- read_csv("data/cleaned/2013_to_2023_cleaned/features_xgboost/jnc7/final_features_for_gb_2017.csv")
+# 
+# fearures_xgb_jnc <- bind_cols(features_xgb_jnc_2013, features_xgb_jnc_2015, features_xgb_jnc_2017) %>% 
+#   dplyr::select(-contains("Count")) %>% 
+#   mutate(across(everything(), ~ str_remove(.x, "_Yes$")))
+# 
+# colnames(fearures_xgb_jnc) <- c("2013", "2015", "2017")
 
 
 # features_2013 <- union(unique(features_en_jnc$`2013`), unique(fearures_xgb_jnc$`2013`))
 # features_2015 <- union(unique(features_en_jnc$`2015`), unique(fearures_xgb_jnc$`2015`))
 # features_2017 <- union(unique(features_en_jnc$`2017`), unique(fearures_xgb_jnc$`2017`))
 
-features_2013 <- intersect(unique(features_en_jnc$`2013`), unique(fearures_xgb_jnc$`2013`))
-features_2015 <- intersect(unique(features_en_jnc$`2015`), unique(fearures_xgb_jnc$`2015`))
-features_2017 <- intersect(unique(features_en_jnc$`2017`), unique(fearures_xgb_jnc$`2017`))
+# features_2013 <- intersect(unique(features_en_jnc$`2013`), unique(fearures_xgb_jnc$`2013`))
+# features_2015 <- intersect(unique(features_en_jnc$`2015`), unique(fearures_xgb_jnc$`2015`))
+# features_2017 <- intersect(unique(features_en_jnc$`2017`), unique(fearures_xgb_jnc$`2017`))
 
 
 # features_2013 <- c(
@@ -42,12 +44,23 @@ features_2017 <- intersect(unique(features_en_jnc$`2017`), unique(fearures_xgb_j
 #   "LBXCOT", "LBXBPB", "LBDEONO", "LBXBCD", "SEQN", "svy_weight_mec", 
 #   "svy_psu", "svy_strata", "bp_control_jnc7")
 
-features_2013 <- c("bp_med_ace", "cc_ckd", "bp_med_diur_thz", "bp_med_angioten",
-                   "cc_diabetes", "bp_med_beta", "bp_med_ccb", "cc_acr",
-                   "demo_age_years", "LBDHDD", "LBDMONO", "LBXRBCSI",
-                   "LBXBSE", "cc_egfr", "SEQN", "svy_weight_mec", 
-                   "svy_psu", "svy_strata", "bp_control_jnc7")
+# features_2013 <- c("bp_med_ace", "cc_ckd", "bp_med_diur_thz", "bp_med_angioten",
+#                    "cc_diabetes", "bp_med_beta", "bp_med_ccb", "cc_acr",
+#                    "demo_age_years", "LBDHDD", "LBDMONO", "LBXRBCSI",
+#                    "LBXBSE", "cc_egfr", "SEQN", "svy_weight_mec", 
+#                    "svy_psu", "svy_strata", "bp_control_jnc7")
 
+features_2013 <- c(
+  "cc_ckd", "cc_cvd_any", "cc_diabetes", "cc_cvd_ascvd",
+  "cc_acr", "demo_race",
+  "LBDHDD", "LBXRBCSI", "cc_smoke", "cc_egfr",
+  "demo_gender", "LBDMONO",
+  "LBXBSE", "weight_change", "INDFMMPI", "LBDBANO",
+  "BMXBMI", "demo_age_years", "LBXBPB", "LBXWBCSI",
+  "SEQN", "svy_weight_mec", 
+  "svy_psu", "svy_strata", "bp_control_jnc7")
+  
+  
 dat_hyp_2013_final <- lapply(dat_hyp_mice_2013, function(x) x[, features_2013])
 
 # features_2015 <- c(
@@ -79,20 +92,41 @@ dat_hyp_2013_final <- lapply(dat_hyp_mice_2013, function(x) x[, features_2013])
 #   "svy_psu", "svy_strata", "bp_control_jnc7"
 # )
 
+# features_2015 <- c(
+#   "bp_med_ace", "bp_med_angioten", "bp_med_diur_thz", "bp_med_beta",
+#   "cc_diabetes", "URXUMS", "weight_change", "cc_acr",
+#   "LBDEONO", "LBXBSE", "demo_age_years", "LBXCOT","SEQN", "svy_weight_mec", 
+#   "svy_psu", "svy_strata", "bp_control_jnc7"
+# )
+
 features_2015 <- c(
-  "bp_med_ace", "bp_med_angioten", "bp_med_diur_thz", "bp_med_beta",
-  "cc_diabetes", "URXUMS", "weight_change", "cc_acr",
-  "LBDEONO", "LBXBSE", "demo_age_years", "LBXCOT","SEQN", "svy_weight_mec", 
-  "svy_psu", "svy_strata", "bp_control_jnc7"
-)
+  "cc_ckd", "cc_cvd_any", "demo_gender", 
+  "cc_cvd_mi", "cc_smoke", "weight_change", "demo_race",
+  "LBDEONO", "LBXBSE", "cc_egfr", "URXUMS",
+  "BMXBMI", "cc_diabetes",
+  "cc_acr", "LBXBPB", "LBXCOT", "LBDBANO","cc_cvd_ascvd", "URXUCR.x.x.x",
+  "INDFMMPI","SEQN", "svy_weight_mec", 
+  "svy_psu", "svy_strata", "bp_control_jnc7")
 
 dat_hyp_2015_final <- lapply(dat_hyp_mice_2015, function(x) x[, features_2015])
 
+# features_2017 <- c(
+#   "bp_med_ace", "bp_med_diur_thz", "bp_med_angioten", "cc_ckd",
+#   "cc_diabetes", "bp_med_beta", "bp_med_ccb", "LBXBPB",
+#   "URXUMS", "cc_acr", "demo_age_years", "cc_cvd_ascvd",
+#   "LBDHDD", "BMXBMI", "LBXHGB", "SEQN", "svy_weight_mec", 
+#   "svy_psu", "svy_strata", "bp_control_jnc7"
+# )
+
 features_2017 <- c(
-  "bp_med_ace", "bp_med_diur_thz", "bp_med_angioten", "cc_ckd",
-  "cc_diabetes", "bp_med_beta", "bp_med_ccb", "LBXBPB",
-  "URXUMS", "cc_acr", "demo_age_years", "cc_cvd_ascvd",
-  "LBDHDD", "BMXBMI", "LBXHGB", "SEQN", "svy_weight_mec", 
+  "cc_ckd", "demo_race", "BMXBMI",
+  "cc_diabetes", "LBXBPB", "cc_egfr",
+  "cc_acr", "URXUMS",
+  "URXUCR.x.x.x", "LBXTHG", "LBDHDD",
+  "LBXRBCSI", "LBXBSE",
+  "LBDMONO", "cc_smoke", "INDFMMPI",
+  "LBXHGB", "cc_cvd_any",
+  "LBDEONO", "cc_hba1c", "SEQN", "svy_weight_mec", 
   "svy_psu", "svy_strata", "bp_control_jnc7"
 )
 
